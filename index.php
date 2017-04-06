f3d8aa07a2a91256cc3eae38d291958a7347f381
Ludi <kubalu2@gmail.com>
2017-04-06 18:43:40 +0200

    [Pilot MIT] Committing basic files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Button - lampa</title>
+	<style type="text/css"></style>
+</head>
+<body>
+	<div class="page_wrapper">
+		<h3>MOBILNY - 192.168.8.121</h3>
+		<div>
+			<a href="http://192.168.8.121/Execute?Command=ToggleNormalMode"><div>Normal Mode</div></a>
+			<a href="http://192.168.8.121/Execute?Command=ToggleAttentionMode"><div>Attention Mode</div></a>
+		</div>
+		<h3>STACJONARNY - 192.168.8.120</h3>
+		<div>
+			<a href="http://192.168.8.120/Execute?Command=ToggleNormalMode"><div>Normal Mode</div></a>
+			<a href="http://192.168.8.120/Execute?Command=ToggleAttentionMode"><div>Attention Mode</div></a>
+		</div>
+	</div>
+</body>
+</html>
